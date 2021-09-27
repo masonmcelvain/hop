70c7a2a33e7739f960b209b20a53e2a872df8a01
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2021-09-26 19:21:44 -0700

    Add Chakra provider to index.js

--- static/index.js
+++ static/index.js
@@ -1,5 +1,11 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { ChakraProvider } from "@chakra-ui/react"
 import App from "../app/App";
 import "./index.css";
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>,
+  document.querySelector("#root")
+);
