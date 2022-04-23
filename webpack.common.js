4798bee50caa875b4f606d57f56c91fb85257531
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2022-04-23 15:38:59 -0700

    webpack: Pass env vars for build target
    And update the package.json scripts to expose options to build for
    chrome and firefox.

--- webpack.common.js
+++ webpack.common.js
@@ -2,65 +2,91 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 const WebpackExtensionManifestPlugin = require("webpack-extension-manifest-plugin");
-const baseManifest = require("./src/chrome/manifest.json");
+const chromeManifest = require("./src/chrome/manifest.json");
+const firefoxManifest = require("./src/firefox/manifest.json");
 
-module.exports = {
-  entry: {
-    app: "./src/index.js",
-  },
-  resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx"],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Hop",
-      meta: {
-        charset: "utf-8",
-        viewport: "width=device-width, initial-scale=1, shrink-to-fit=no",
-        "theme-color": "#000000",
-      },
-      manifest: "manifest.json",
-      filename: "index.html",
-      template: "./src/index.html",
-      hash: true,
-    }),
-    new CopyPlugin({
-      patterns: [
+const CHROME = "chrome";
+const FIREFOX = "firefox";
+
+module.exports = (env) => {
+  if (![CHROME, FIREFOX].includes(env.target)) {
+    throw new Error(`Invalid target: ${env.target}`);
+  }
+
+  let iconPattern;
+  let baseManifest;
+  switch(env.target) {
+    case CHROME:
+      iconPattern = {
+        from: "src/chrome/icons",
+        to: "icons",
+      };
+      baseManifest = chromeManifest;
+      break;
+    case FIREFOX:
+      iconPattern = {
+        from: "src/firefox/icon.svg",
+        to: "icon.svg",
+      };
+      baseManifest = firefoxManifest;
+      break;
+  }
+
+  return {
+    entry: {
+      app: "./src/index.js",
+    },
+    resolve: {
+      extensions: [".js", ".jsx", ".ts", ".tsx"],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: "Hop",
+        meta: {
+          charset: "utf-8",
+          viewport: "width=device-width, initial-scale=1, shrink-to-fit=no",
+          "theme-color": "#000000",
+        },
+        manifest: "manifest.json",
+        filename: "index.html",
+        template: "./src/index.html",
+        hash: true,
+      }),
+      new CopyPlugin({
+        patterns: [
+          iconPattern,
+        ],
+      }),
+      new WebpackExtensionManifestPlugin({
+        config: {
+          base: baseManifest,
+        },
+      }),
+    ],
+    module: {
+      rules: [
         {
-          from: "src/chrome/icons",
-          to: "icons",
+          test: /\.(js|jsx|ts|tsx)$/,
+          exclude: /node_modules/,
+          loader: "babel-loader",
+          options: {
+            plugins: ["@babel/transform-runtime"],
+          },
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(png|svg|jpg|gif)$/,
+          use: ["file-loader"],
         },
       ],
-    }),
-    new WebpackExtensionManifestPlugin({
-      config: {
-        base: baseManifest,
-      },
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx|ts|tsx)$/,
-        exclude: /node_modules/,
-        loader: "babel-loader",
-        options: {
-          plugins: ["@babel/transform-runtime"],
-        },
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: ["file-loader"],
-      },
-    ],
-  },
-  output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
+    },
+    output: {
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
+      clean: true,
+    },
+  };
 };
