e15e3c7c035867ab5920d006f5f1953f593c4015
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2022-12-11 13:09:39 -0500

    fix: drop image refetching
    Deletes the image refetching feature when the image is missing.
    
    The browser does a good job of fetcing the image url for us, so let's drop the extra baggage of refetching it ourselves.

--- src/components/CardImage.tsx
+++ src/components/CardImage.tsx
@@ -1,34 +1,13 @@
 import * as React from "react";
 import { Image as FeatherImageIcon } from "react-feather";
 import { Icon, Image } from "@chakra-ui/react";
-import { LinkAction, LinkData } from "../contexts/Links/reducer";
-import { LinksContext } from "../contexts/Links";
-import { getHighestResFaviconUrl } from "../lib/getFavicon";
+import { LinkData } from "../contexts/Links/reducer";
 
 type CardImageProps = {
   linkData: LinkData;
 };
 export default function CardImage({ linkData }: CardImageProps): JSX.Element {
-  const { dispatch } = React.useContext(LinksContext);
-  const { id, name, url, imageUrl } = linkData;
-
-  React.useEffect(() => {
-    if (!imageUrl) {
-      const fetchAndSetFaviconUrl = async () => {
-        const faviconUrl = await getHighestResFaviconUrl(url);
-        faviconUrl &&
-          dispatch({
-            type: LinkAction.UPDATE_IMAGE_URL,
-            payload: {
-              url: faviconUrl.toString(),
-              linkId: id,
-            },
-          });
-      };
-      fetchAndSetFaviconUrl();
-    }
-  }, [dispatch, id, imageUrl, url]);
-
+  const { name, imageUrl } = linkData;
   return imageUrl ? (
     <Image src={imageUrl} alt={name} w={8} />
   ) : (
