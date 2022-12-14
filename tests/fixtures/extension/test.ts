c72ac0f56f39f38e10d943f4608507a51d997030
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2022-12-14 10:31:28 -0500

    fix(test): extract a helper to add links

--- tests/fixtures/extension/test.ts
+++ tests/fixtures/extension/test.ts
@@ -40,14 +40,18 @@ export const test = base.extend<{
   pageWithLinks: async ({ context, extensionId, links }, use) => {
     const [page] = context.pages();
     await page.goto(`chrome-extension://${extensionId}/index.html`);
-    for (const { name, url, imageUrl } of links) {
-      await page.getByRole("button", { name: "Create new link" }).click();
-      await page.getByPlaceholder("Name").fill(name);
-      await page.getByPlaceholder("Link URL").fill(url);
-      await page.getByPlaceholder("Image URL").fill(imageUrl);
-      await page.getByRole("button", { name: "Create", exact: true }).click();
+    for (const link of links) {
+      await addLink(page, link);
     }
     await use(page);
   },
 });
 export const expect = test.expect;
+
+async function addLink(page: Page, link: typeof links[0]) {
+  await page.getByRole("button", { name: "Create new link" }).click();
+  await page.getByPlaceholder("Name").fill(link.name);
+  await page.getByPlaceholder("Link URL").fill(link.url);
+  await page.getByPlaceholder("Image URL").fill(link.imageUrl);
+  await page.getByRole("button", { name: "Create", exact: true }).click();
+}
