ce42517d935389d5ccf09eb870e1da4d590eb9aa
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2022-12-23 08:47:29 -0800

    feat: use `env-cmd` for env vars
    Cleans up the build/dev scripts by using the `env-cmd` tool to manage environment variables. Variables can be overridden using the `.env` file.

---
+++ .env-cmdrc.js
@@ -0,0 +1,34 @@
+const { parsed: overrides } = require("dotenv").config();
+
+module.exports = {
+  "dev-chrome": {
+    NODE_ENV: "development",
+    TARGET: "chrome",
+    ...overrides,
+  },
+  "dev-firefox": {
+    NODE_ENV: "development",
+    TARGET: "firefox",
+    ...overrides,
+  },
+  "preview-firefox": {
+    // WEB_EXT_FIREFOX: "flatpak:org.mozilla.firefox",
+    WEB_EXT_FIREFOX_PROFILE: "tmp-web-ext-profile",
+    // firefox can't connect when we save profile changes?
+    // https://github.com/mozilla/web-ext/issues/932
+    WEB_EXT_KEEP_PROFILE_CHANGES: false,
+    WEB_EXT_PROFILE_CREATE_IF_MISSING: true,
+    WEB_EXT_SOURCE_DIR: "dist/firefox",
+    ...overrides,
+  },
+  "prod-chrome": {
+    NODE_ENV: "production",
+    TARGET: "chrome",
+    ...overrides,
+  },
+  "prod-firefox": {
+    NODE_ENV: "production",
+    TARGET: "firefox",
+    ...overrides,
+  },
+};
