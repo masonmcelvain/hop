b9bfd39ea15cf062e721ff6b35f9c9797a95e8d3
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2023-04-30 09:48:33 -0700

    refactor(playwright): simplify config
    And reduce test timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,16 +6,11 @@ import { devices } from "@playwright/test";
  */
 const config: PlaywrightTestConfig = {
    testDir: "./tests",
-   /* Maximum time one test can run for. */
-   timeout: 30 * 1000,
+   /* Maximum time (in ms) one test can run for. */
+   timeout: 10000,
    expect: {
-      /**
-       * Maximum time expect() should wait for the condition to be met.
-       * For example in `await expect(locator).toHaveText();`
-       */
       timeout: 5000,
    },
-   /* Run tests in files in parallel */
    fullyParallel: true,
    /* Fail the build on CI if you accidentally left test.only in the source code. */
    forbidOnly: !!process.env.CI,
@@ -26,11 +21,6 @@ const config: PlaywrightTestConfig = {
    reporter: "html",
    /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
    use: {
-      /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
-      actionTimeout: 0,
-      /* Base URL to use in actions like `await page.goto('/')`. */
-      // baseURL: 'http://localhost:3000',
-
       /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
       trace: "on-first-retry",
    },
@@ -50,15 +40,6 @@ const config: PlaywrightTestConfig = {
          },
       },
    ],
-
-   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
-   // outputDir: 'test-results/',
-
-   /* Run your local dev server before starting the tests */
-   // webServer: {
-   //   command: 'npm run start',
-   //   port: 3000,
-   // },
 };
 
 export default config;
